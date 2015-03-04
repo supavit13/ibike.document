39b3448b56a8907a8dfdece7f211be03b85dbc14
Sirisak Lueangsaksri <spywhere@me.com>
2015-03-04 17:25:15 +0700

    Optimise Thai encoding - Optimise Thai encoding - Can be run on python2 and python3

--- opus.py
+++ opus.py
@@ -15,6 +15,7 @@
 import datetime
 import sys
 import os
+import re
 
 
 class ControllerParser:
@@ -75,33 +76,10 @@ class CodeWriter:
         self.writeLine(self.translator.translate(s))
 
     def thaicode(self, s):
-        result = ""
-        thstart = []
-        thstop = []
-        state = "en"
-        for i in range(len(s)):
-            c = s[i]
-            if ord(c) >= 128:
-                if state == "en":
-                    thstart.append(i)
-                    state = "th"
-            elif state == "th":
-                thstop.append(i)
-                state = "en"
-        if state == "th":
-            thstop.append(len(s))
-        laststop = 0
-        for i in range(len(thstart)):
-            result += s[laststop:thstart[i]]
-            result += "{\\thi "
-            result += s[thstart[i]:thstop[i]]
-            result += "}"
-            laststop = thstop[i]
-        result += s[laststop:]
-        return result
+        return re.sub("([^\\x00-\\xff]+)", "{\\thi \\1}", s)
 
     def writeReport(self):
-        print "Exporting as report..."
+        print("Exporting as report...")
         self.writeHeader()
         self.writeReportMainCover()
         self.writeReportInsideCover()
@@ -116,7 +94,7 @@ class CodeWriter:
         self.writeEnding()
 
     def writeProposal(self):
-        print "Exporting as proposal..."
+        print("Exporting as proposal...")
         self.writeHeader()
         self.writeProposalCover()
         self.writeApprovalForm(proposal=True)
